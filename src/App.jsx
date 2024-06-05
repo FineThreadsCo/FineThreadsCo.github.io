287a6142665a2b329301756bbed873918d002536
jwt2706 <jwt2home@gmail.com>
2024-06-05 09:16:09 -0400

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./components/Home";
+import AboutPage from "./components/About";
+import ServicesPage from "./components/Services";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/main.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-      {/* use the bootsraps toast thing for the popup confirmation */}
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </div>
+      {/* NOTE: use the bootsraps toast thing for the popup confirmation */}
+    </Router>
   );
 }
 
